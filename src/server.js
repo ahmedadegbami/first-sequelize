9fe3ab8b0fd8b5beb6235e033b790f4ec7738cf9
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-05-24 16:50:47 +0200

    updated

--- src/server.js
+++ src/server.js
@@ -4,19 +4,33 @@ import { testDB } from "./db/index.js";
 import sequelize from "./db/index.js";
 import Product from "./db/models/products.js";
 import Review from "./db/models/reviews.js";
+import productRouter from "./services/products/index.js";
+import reviewRouter from "./services/reviews/index.js";
+
+import {
+  badRequestErrorHandler,
+  notFoundErrorHandler,
+  genericErrorHandler
+} from "./errorHandlers.js";
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+server.use("/products", productRouter);
+server.use("/reviews", reviewRouter);
 
 const { PORT = 5001 } = process.env;
+
+server.use(badRequestErrorHandler);
+server.use(notFoundErrorHandler);
+server.use(genericErrorHandler);
 
 const initalize = async () => {
   try {
     server.listen(PORT, async () => {
       console.log("✅ Server is listening on port " + PORT);
       await testDB();
-      await sequelize.sync({ alter: false });
+      await sequelize.sync({ alter: true });
       //   await Product.sync({ force: false });
       //   await Review.sync({ force: false });
     });
